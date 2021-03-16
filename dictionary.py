e16ff5457dc169af8d0d4c708811d7ede86e7f6c
Kevin Falconett <kevin.fal360@gmail.com>
2021-03-15 18:08:23 -0700

    documentation for dictionary.py, added separate_words() and complete_wordd() to parser.py, TODO: DEBUG

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,29 @@
 
 """
-    Dictionary Class
+    Dictionary Class:
+        Holds a list (word_list) of words as its word bank. Only words that the program recognizes
+        are within this word list. Can perfom a word lookup to see if a word is within the given dictionary.
     Arguments: (str) source: name of file with list of words to use as dictionary
 """
 class dictionary:
+    """
+             Upon initiation, construct word list to use as dictionary.
+        Arguments:
+            source: (str) name of file that contains list of words. 
+    """
     def __init__(self,source):
         self.source = source
         self.word_list = self.read_word_list(source)
     """
-        Reads the 
+        Reads the file that holds all of the words for the dictionary.
+        File should be formatted such that every line has one word, and
+        only that word until the next line.
+
+        Arguments:
+            source: (str): name of file that contains list of words (must incluse .txt at end)
+
+        Returns:
+            str[] of all lines in the source text file, with each line in the file being its own word
     """
     def read_word_list(self,source):
         returned = []
@@ -16,6 +31,17 @@ class dictionary:
         for word in file:   # every line in the file will be one word
             returned.append(word.rstrip())
         return returned
+
+    """
+        Iterates through word_list to see if a word is present in the dictionary, returns
+        True if the word is found, false otherwise
+
+        Arguments:
+            string: (str) word to look up in dictionary
+
+        Returns:
+            True if the string is in word_list, False if it is not.
+    """
     def word_lookup(self,string):
         for word in self.word_list:
             # if string is a word in the dictionary
@@ -25,14 +51,15 @@ class dictionary:
         return False
 
 
-
-
 """
+    create_word_dictionary():
+            Creates a text file with a list of words from the data set given from korpus. 
+            Used to create simpilfied_2014.txt and macau_2015.txt. Input file should be inputted such that
+            there are at least 2 columns of data, with the word being the sescond one
     Arguments:
         (str) file_title: title of text file to get from.
                 Specified to parse through specific data set from korpusdownload
         (str) output: what to name output text file, must include .txt
-    Creates a text file with a list
 """
 def create_word_dictionary(file_title,output):
     words = []
--- parser.py
+++ parser.py
@@ -1,11 +1,68 @@
 
 from dictionary import dictionary
+
+"""
+
+"""
 class parser:
     def __init__(self,source):
         self.dictionary = dictionary(source)
 
+    """
+        separates all of the words in a sentence or given input
+    """
+    def separate_words(self, input):
+        returned = []
+        i = 0
+        while i < len(input):
+            print("loop call")
+            print(input[i:len(input)])
+
+            if(input[i] == ' '):
+                i+= 1
+                continue
+
+            i2 = self.complete_word( input, i) # set i2 to index of where word ends
+
+            if(i2 == None):
+                print("NONE")
+                returned.append("/?" + input[i] + "?/")  # character not found in dictionary
+                i2 = i
+                continue
+            returned.append(input[i:i2])       # word is input[i:i2], add to list
+            i = i2                         # i = i2 + 1 to not pass through covered characters
+        return returned
+
+    """
+        Arguments:
+            index: index of where word starts
+
+        Returns:
+            (int) index of where the word ends, 
+            None if character at index is not a word
+    """
+    def complete_word(self, input, index):
+        word = input[index]
+        print("called complete_word: " + input[index])
+        if(not self.dictionary.word_lookup(word)):
+            if(self.dictionary.word_lookup(word + input[index + 1])):
+                word = word + input[index+1]
+                index+=1
+            else:
+                return None
+        i = index
+        while i < len(input) -1 and self.dictionary.word_lookup(word):
+            i+=1
+            word += input[i]
+        return index + len(word) 
+    
+        
+
 if (__name__ == "__main__"):
     d = dictionary("simplified_2014.txt")
-    print(d.word_lookup("没有"))
+    string = "你叫"
+    print(d.word_lookup(string))
     parser = parser("simplified_2014.txt")
+    print(  parser.separate_words(string)
+)
 
