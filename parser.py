72455531ce09f1a4a45287ff84913e978b71eafd
Kevin Falconett <kevin.fal360@gmail.com>
2021-03-15 18:42:44 -0700

    parser separate_words cand complete_word() function, need more testing. TODO segment into string

--- parser.py
+++ parser.py
@@ -23,13 +23,18 @@ class parser:
                 continue
 
             i2 = self.complete_word( input, i) # set i2 to index of where word ends
-
+            print(i2)
             if(i2 == None):
                 print("NONE")
                 returned.append("/?" + input[i] + "?/")  # character not found in dictionary
                 i2 = i
                 continue
             returned.append(input[i:i2])       # word is input[i:i2], add to list
+
+            # this happens when word is one character
+            if(i == i2):
+                print("ERROR, THIS IS WHY")
+                i2 += 1
             i = i2                         # i = i2 + 1 to not pass through covered characters
         return returned
 
@@ -51,18 +56,34 @@ class parser:
             else:
                 return None
         i = index
+        # if last character in input string
+        if(i == len(input) - 1):
+            return len(input)
         while i < len(input) -1 and self.dictionary.word_lookup(word):
+            print("word: " +word)
             i+=1
             word += input[i]
-        return index + len(word) 
+            print("appended word: " + word)
+            print(self.dictionary.word_lookup(word))
+        print("word to add: " + word)
+        # exit conition was i < len(input) 
+        if(i == len(input) - 1):
+            # if was a word
+            if(self.dictionary.word_lookup(word)):
+                return len(input)
+            else:
+                # word was not a valid word
+                return len(input) - 1
+        return index + len(word) - 1
     
         
 
 if (__name__ == "__main__"):
     d = dictionary("simplified_2014.txt")
-    string = "你叫"
+    string = "北京在中国北方广州在中国南方"
     print(d.word_lookup(string))
     parser = parser("simplified_2014.txt")
-    print(  parser.separate_words(string)
-)
+    print(  parser.separate_words(string))
+    print("北京　在　中国　北方；　广州　在　中国　南方")
+
 
